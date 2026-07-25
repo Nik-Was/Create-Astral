5952a01b1acfcd63692d1312d9a7c1af198da20b
NikWas <150386333+Nik-Was@users.noreply.github.com>
2026-07-25 22:17:11 +0300

    Add missing mineable tags to leaves, ice, moss variants

--- kubejs/server_scripts/misc/chipped_tags.js
+++ kubejs/server_scripts/misc/chipped_tags.js
@@ -14,5 +14,28 @@
     jungles.forEach(log => { event.add("minecraft:mineable/axe", log); });
     oaks.forEach(log => { event.add("minecraft:mineable/axe", log); });
     spruces.forEach(log => { event.add("minecraft:mineable/axe", log); });
+
+    const acacia_leaves = event.get("chipped:acacia_leaves").getObjectIds();
+    const birch_leaves = event.get("chipped:birch_leaves").getObjectIds();
+    const dark_oak_leaves = event.get("chipped:dark_oak_leaves").getObjectIds();
+    const jungle_leaves = event.get("chipped:jungle_leaves").getObjectIds();
+    const oak_leaves = event.get("chipped:oak_leaves").getObjectIds();
+    const spruce_leaves = event.get("chipped:spruce_leaves").getObjectIds();
+    acacia_leaves.forEach(leaf => { event.add("minecraft:mineable/hoe", leaf); });
+    birch_leaves.forEach(leaf => { event.add("minecraft:mineable/hoe", leaf); });
+    dark_oak_leaves.forEach(leaf => { event.add("minecraft:mineable/hoe", leaf); });
+    jungle_leaves.forEach(leaf => { event.add("minecraft:mineable/hoe", leaf); });
+    oak_leaves.forEach(leaf => { event.add("minecraft:mineable/hoe", leaf); });
+    spruce_leaves.forEach(leaf => { event.add("minecraft:mineable/hoe", leaf); });
+
+    const ices = event.get("chipped:ice").getObjectIds();
+    const packed_ices = event.get("chipped:packed_ice").getObjectIds();
+    const blue_ices = event.get("chipped:blue_ice").getObjectIds();
+    ices.forEach(ice => { event.add("minecraft:mineable/pickaxe", ice); });
+    packed_ices.forEach(ice => { event.add("minecraft:mineable/pickaxe", ice); });
+    blue_ices.forEach(ice => { event.add("minecraft:mineable/pickaxe", ice); });
+
+    const mosses = event.get("chipped:moss_block").getObjectIds();
+    mosses.forEach(moss => { event.add("minecraft:mineable/hoe", moss); });
   });
 })();
