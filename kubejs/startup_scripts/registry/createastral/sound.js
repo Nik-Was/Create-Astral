00ebf8074e6f67ca7e342433e2a99d52d3f8c9c6
NikWas <150386333+Nik-Was@users.noreply.github.com>
2026-09-22 12:42:21 +0300

    Fix cogwheel skull sound not getting registered properly

--- kubejs/startup_scripts/registry/createastral/sound.js
+++ kubejs/startup_scripts/registry/createastral/sound.js
@@ -1,5 +1,5 @@
 (function createAstralSoundRegistry(){
   onEvent("sound.registry", (event) => {
-    event.create("createastral:stop_sound");
+    event.register("createastral:stop_sound");
   });
 })();
